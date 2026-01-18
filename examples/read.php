c1194c6933e605cdb65e15bc26653c65759e6209
Vic Cherubini <vcherubini@gmail.com>
2026-01-18 06:49:46 -0600

    wip

--- examples/read.php
+++ examples/read.php
@@ -2,12 +2,7 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-// $tags = new \OneToMany\ExifTools\Reader\ExifTagReader()->read('/Users/vic/Downloads/ao-smith-label.jpg');
-$tags = new OneToMany\ExifTools\Reader\ExifTagReader()->read('/Users/vic/Downloads/ao-smith-label.jpg');
+$tags = new \OneToMany\ExifTools\Reader\ExifTagReader()->read('/Users/vic/Downloads/ao-smith-label.jpg');
+// $tags = new OneToMany\ExifTools\Reader\ExifTagReader()->read('/Users/vic/Downloads/ao-smith-label.jpg');
 
-print_r($tags->toArray());
-// var_dump($tags->get('ComponentsConfiguration')->isList());
-// var_dump($tags->has('COMPUTED'));
-// var_dump($tags->get('COMPUTED')->getValue()->has('height'));
-// var_dump($tags->get('HostComputer')->getValue());
-// print_r($tags->get('COMPUTED')->getValue());
+// print_r($tags->all());
