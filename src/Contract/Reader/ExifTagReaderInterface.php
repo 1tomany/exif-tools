1522985a42edb341a61703dea556d6477fb121d6
Vic Cherubini <vcherubini@gmail.com>
2026-01-17 21:27:17 -0600

    wip

--- src/Contract/Reader/ExifTagReaderInterface.php
+++ src/Contract/Reader/ExifTagReaderInterface.php
@@ -2,7 +2,6 @@
 
 namespace OneToMany\ExifTools\Contract\Reader;
 
-use OneToMany\ExifTools\Contract\Record\ExifMapInterface;
 use OneToMany\ExifTools\Record\ExifMap;
 
 interface ExifTagReaderInterface
