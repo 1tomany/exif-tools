948897136be93e60d1b7f1f2cf2b7c9706c3f140
Vic Cherubini <vcherubini@gmail.com>
2026-01-17 20:50:18 -0600

    wip

--- examples/read.php
+++ examples/read.php
@@ -3,11 +3,9 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 // $tags = new \OneToMany\ExifTools\Reader\ExifTagReader()->read('/Users/vic/Downloads/ao-smith-label.jpg');
-$tags = new OneToMany\ExifTools\Reader\ExifTagReader()->read('/Users/vic/Downloads/IMG_3470.jpeg');
+$tags = new OneToMany\ExifTools\Reader\ExifTagReader()->read('/Users/vic/Downloads/ao-smith-label.jpg');
 
-// print_r($tags->all());
-var_dump($tags->has('GPSAltitudeRef'));
-var_dump($tags->has('COMPUTED'));
-
-var_dump($tags->get('GPSDestBearing')->getValue());
+// var_dump($tags->get('ComponentsConfiguration')->isList());
+// var_dump($tags->has('COMPUTED'));
+// var_dump($tags->get('GPSDestBearing')->getValue());
 // print_r($tags->get('COMPUTED')->getValue());
