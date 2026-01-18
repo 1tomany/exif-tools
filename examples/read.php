d93dbe1aca89c7e23f20bbc39a8e0999db6192e7
Vic Cherubini <vcherubini@gmail.com>
2026-01-18 07:11:12 -0600

    wip

--- examples/read.php
+++ examples/read.php
@@ -5,6 +5,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 $file = '/Users/vic/Downloads/IMG_3470.jpeg';
 // $file = '/Users/vic/Downloads/ao-smith-label.jpg';
 
-$tags = new \OneToMany\ExifTools\Reader\ExifTagReader()->read($file);
+$tags = new OneToMany\ExifTools\Reader\ExifTagReader()->read($file);
 
 print_r($tags->toArray());
