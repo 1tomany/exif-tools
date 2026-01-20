059fe83079b6c2d527e692589a87895a32bc6776
Vic Cherubini <vcherubini@gmail.com>
2026-01-20 12:47:11 -0600

    wip

--- tests/Record/GpsValueTest.php
+++ tests/Record/GpsValueTest.php
@@ -9,14 +9,18 @@ use PHPUnit\Framework\TestCase;
 
 use function random_int;
 
+use const PHP_INT_MAX;
+
 #[Group('UnitTests')]
 #[Group('RecordTests')]
 final class GpsValueTest extends TestCase
 {
     public function testConstructorRequiresValidLatitude(): void
     {
-        // Random latitude less than -90 or greater than +90
-        $latitude = (0 === random_int(0, 1) ? -1 : 1) * (random_int(90, 360) + (random_int(1, 100) / random_int(1, 100)));
+        $signFlipper = 0 === random_int(0, 1) ? -1 : 1;
+
+        // Latitude less than -90 or greater than +90
+        $latitude = ($signFlipper * random_int(90, 180));
         $this->assertTrue($latitude < -90 || $latitude > 90);
 
         $this->expectException(InvalidArgumentException::class);
@@ -27,8 +31,10 @@ final class GpsValueTest extends TestCase
 
     public function testConstructorRequiresValidLongitude(): void
     {
-        // Random longitude less than -180 or greater than +180
-        $longitude = (0 === random_int(0, 1) ? -1 : 1) * (random_int(180, 360) + (random_int(1, 100) / random_int(1, 100)));
+        $signFlipper = 0 === random_int(0, 1) ? -1 : 1;
+
+        // Longitude less than -180 or greater than +180
+        $longitude = $signFlipper * random_int(180, 360);
         $this->assertTrue($longitude < -180 || $longitude > 180);
 
         $this->expectException(InvalidArgumentException::class);
@@ -39,13 +45,29 @@ final class GpsValueTest extends TestCase
 
     public function testConstructorRequiresValidAltitude(): void
     {
-        // Random altitude lower than the depth of the Mariana Trench
-        $altitude = -1 * random_int(GpsValue::MARIANA_TRENCH_DEPTH+1, \PHP_INT_MAX);
+        // Altitude lower than the depth of the Mariana Trench
+        $altitude = -1 * random_int(GpsValue::MARIANA_TRENCH_DEPTH + 1, GpsValue::MARIANA_TRENCH_DEPTH + 1_000_000);
         $this->assertTrue($altitude < GpsValue::MARIANA_TRENCH_DEPTH);
 
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('The altitude "'.GpsValue::toDecimal($altitude, 2).'" must be greater than '.GpsValue::MARIANA_TRENCH_DEPTH.'.');
 
         new GpsValue(null, null, $altitude);
+    }
+
+    public function testIsNotValidWithNullLatitude(): void
+    {
+        $gps = new GpsValue(null, random_int(-180, 180));
+
+        $this->assertNull($gps->latitude);
+        $this->assertFalse($gps->isValid());
+    }
+
+    public function testIsNotValidWithNullLongitude(): void
+    {
+        $gps = new GpsValue(random_int(-90, 90), null);
+
+        $this->assertNull($gps->longitude);
+        $this->assertFalse($gps->isValid());
     }
 }
