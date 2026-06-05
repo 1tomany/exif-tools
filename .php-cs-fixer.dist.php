ac337b32028e0d33a2e81667a3c9142d8ebe6e18
Vic Cherubini <vcherubini@gmail.com>
2026-06-05 12:56:41 -0500

    [#7] Setup `.build` directory (#8)
    resolves #7

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,36 +7,25 @@ use PhpCsFixer\Runner\Parallel\ParallelConfig;
 $finder = new Finder();
 
 $finder->in([
-    './',
+    './examples/',
     './src/',
     './tests/',
 ]);
 
-$finder->exclude([
-    'config',
-    'vendor',
-]);
-
-$config = new Config()->setParallelConfig(...[
-    'config' => new ParallelConfig(4),
-]);
-
-$config->setFinder($finder);
-$config->setRules([
-    '@Symfony' => true,
-    'global_namespace_import' => [
-        'import_classes' => false,
-        'import_constants' => true,
-        'import_functions' => true,
-    ],
-    'phpdoc_align' => [
-        'align' => 'left',
-    ],
-    'phpdoc_to_comment' => [
-        'ignored_tags' => [
-            'disregard',
+$config = new Config()
+    ->setFinder($finder)
+    ->setParallelConfig(new ParallelConfig(4))
+    ->setCacheFile('./.build/php-cs-fixer.cache')
+    ->setRules([
+        '@Symfony' => true,
+        'global_namespace_import' => [
+            'import_classes' => false,
+            'import_constants' => true,
+            'import_functions' => true,
         ],
-    ],
-]);
+        'phpdoc_align' => [
+            'align' => 'left',
+        ],
+    ]);
 
 return $config;
