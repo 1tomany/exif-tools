130c8a94473f8eb5fc43e5d37b745ea51b14495b
Vic Cherubini <vcherubini@gmail.com>
2026-06-11 13:59:29 -0500

    [#14] Add PHPStan assertions for `GpsValue::isValid()` (#15)
    resolves #14

--- src/Record/GpsValue.php
+++ src/Record/GpsValue.php
@@ -155,7 +155,12 @@ final readonly class GpsValue
 
     /**
      * @phpstan-assert-if-true float $this->latitude
+     * @phpstan-assert-if-true float $this->getLatitude()
+     * @phpstan-assert-if-true numeric-string $this->getLatitudeDecimal()
      * @phpstan-assert-if-true float $this->longitude
+     * @phpstan-assert-if-true float $this->getLongitude()
+     * @phpstan-assert-if-true numeric-string $this->getLongitudeDecimal()
+     * @phpstan-assert-if-true array{0: numeric-string, 1: numeric-string} $this->all()
      */
     public function isValid(): bool
     {
